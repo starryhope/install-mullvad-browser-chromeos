127f23eeed94838f7ddaf2d154a173233219e921
Jim Mendenhall <git@starryhope.com>
2023-09-16 19:48:36 -0400

    adding extra onscreen feedback to user

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Install necessary dependencies
+echo "Installing necessary dependencies..."
 sudo apt update
 sudo apt install -y gpg libdbus-glib-1-2
+echo "Dependencies installed successfully."
 
 # Working directory
 WORK_DIR="/tmp/mullvad_browser"
@@ -20,34 +21,43 @@ mkdir -p "$WORK_DIR"
 cd "$WORK_DIR"
 
 # Download and import Tor Browser Developers' signing key
+echo "Importing Tor Browser Developers' signing key..."
 gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org
 
 # Set trust level to ultimate
 echo -e "trust\n5\ny\nquit" | gpg --command-fd 0 --edit-key EF6E286DDA85EA2A4BA7DE684E2C6E8793298290
 
 # Download files using wget
+echo "Downloading Mullvad Browser and its signature..."
 wget "$MULLVAD_URL" -O "$TAR_FILE"
 wget "$SIGNATURE_URL" -O "$SIG_FILE"
+echo "Download complete."
 
 # Verify the integrity of the downloaded file
+echo "Verifying the integrity of the downloaded file..."
 gpg --verify "$SIG_FILE" "$TAR_FILE"
 if [ $? -ne 0 ]; then
     echo "GPG verification failed."
     exit 1
 fi
+echo "Verification successful."
 
 # Extract to ~/.local/share
+echo "Extracting the downloaded browser..."
 INSTALL_DIR="${HOME}/.local/share"
 mkdir -p "$INSTALL_DIR"
 tar xf "$TAR_FILE" -C "$INSTALL_DIR"
+echo "Extraction complete."
 
-# Set permissions for ~/.local/share/mullvad-browser to be readable and executable by the user
+# Set permissions
+echo "Setting permissions..."
 chmod -R 755 "$INSTALL_DIR"
+echo "Permissions set."
 
 # Create a desktop file
+echo "Creating a menu shortcut..."
 DESKTOP_FILE_DIR="${HOME}/.local/share/applications"
 DESKTOP_FILE_PATH="${DESKTOP_FILE_DIR}/start-mullvad-browser.desktop"
-
 mkdir -p "$DESKTOP_FILE_DIR"
 
 cat > "$DESKTOP_FILE_PATH" <<EOL
@@ -64,4 +74,6 @@ StartupWMClass=Mullvad Browser
 EOL
 
 # Cleanup
+echo "Cleaning up temporary files..."
 rm -rf "$WORK_DIR"
+echo "Installation complete!"
